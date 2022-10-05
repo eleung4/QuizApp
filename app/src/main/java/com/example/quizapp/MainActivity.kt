a5b82946205208f5a3fc7b02b820cdaf34b1b082
ellen <ellenleung4@gmail.com>
2022-10-05 10:07:34 -0700

    wired widgets n stuff

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        val TAG = "MainActivity"
+    }
+
+    private lateinit var quiz : Quiz
+    lateinit var buttonTrue : Button
+    lateinit var buttonFalse : Button
+    lateinit var textView : TextView
+    lateinit var textView2 : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadQuestions()
+        wireWidgets()
+
+    }
+
+
+
+
+    private fun wireWidgets() {
+        buttonTrue = findViewById(R.id.button_MainActivity_true)
+        buttonFalse = findViewById(R.id.button_MainActivity_false)
+        textView = findViewById(R.id.textView_MainActivity_question)
+        textView2 = findViewById(R.id.textView2_MainActivity_points)
+    }
+
+    private fun loadQuestions() {
+        val inputStream = resources.openRawResource(R.raw.questions)
+
+        val jsonString = inputStream.bufferedReader().use {
+            // the last line of the use function is returned
+            it.readText()
+        }
+        Log.d(TAG, "onCreate: $jsonString")
+
+        // alternative way of writing the above line with the use lambda
+//        val bufferedReader = inputStream.bufferedReader()
+//        val jsonString = bufferedReader.readText()
+
+        val gson = Gson()
+        // gson needs to know what kind of list you are converting to
+        // typetoken tells gson it will be a List<Question>
+
+        val type = object : TypeToken<List<Question>>() { }.type
+        val questions = gson.fromJson<List<Question>>(jsonString, type)
+
     }
 }
---
+++ app/src/main/java/com/example/quizapp/Quiz.kt
@@ -0,0 +1,9 @@
+package com.example.quizapp
+
+class Quiz(val questions: List<Question>) {
+    //variables to track score, current question
+
+    //functions to check if there's another question,
+    //give the next question, check the answer
+    //give the final score, reset the quiz?, shuffle questions?
+}
